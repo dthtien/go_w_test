a8adc7c225c87ba429ba32f1bdc61c8c43751193
dthtien <tien.dau@employmenthero.com>
2021-08-30 20:31:11 +0700

    ft(go_w_test): day 5 fix coc

--- structs/perimeter.go
+++ structs/perimeter.go
@@ -23,12 +23,12 @@ type Rectangle struct {
   Height float64
 }
 
-func (regtangle Rectangle) Area() float64 {
-  return regtangle.Height * regtangle.Width
+func (r Rectangle) Area() float64 {
+  return r.Height * r.Width
 }
 
-func (regtangle Rectangle) Perimeter() float64 {
-  return (regtangle.Height + regtangle.Width) * 2
+func (r Rectangle) Perimeter() float64 {
+  return (r.Height + r.Width) * 2
 }
 
 type Triangle struct {
